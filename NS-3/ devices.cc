94d7ebc555a40470f37a7f24e11286026918d4fd
olyashik <102978625+olyashik@users.noreply.github.com>
2025-10-23 18:58:39 +0300

    Update devices.cc
    Замена "идеального" канала связи на беспроводной (создана модель потерь с учётом затухания)

--- NS-3/ devices.cc
+++ NS-3/ devices.cc
@@ -5,6 +5,8 @@
 #include "ns3/applications-module.h"
 #include "ns3/internet-module.h"
 #include "ns3/log.h"
+#include "ns3/propagation-loss-model.h"
+#include "ns3/propagation-delay-model.h"
 
 using namespace ns3;
 using namespace lorawan;
@@ -14,9 +16,11 @@ NS_LOG_COMPONENT_DEFINE ("LoraThreeDevices");
 int main (int argc, char *argv[])
 {
   // Параметры по умолчанию
-  int nDevices = 3;           // Количество устройств
-  double simulationTime = 3600; // Время симуляции в секундах (1 час)
-  double appPeriod = 600;     // Период отправки данных (10 минут)
+  int nDevices = 3;
+  double simulationTime = 3600;
+  double appPeriod = 600;
+  double noiseFigure = 7.0; // Коэффициент шума в dB
+  double frequency = 868e6; // Частота 868 MHz
 
   // Настройка логирования
   LogComponentEnable ("LoraThreeDevices", LOG_LEVEL_INFO);
@@ -27,16 +31,35 @@ int main (int argc, char *argv[])
   // Создание узлов 
   NodeContainer endDevices;
   endDevices.Create (nDevices);
-
   NodeContainer gateways;
-  gateways.Create (1);  // Один шлюз
+  gateways.Create (1);
 
-  // Настройка мобильности
+  // Настройка реального беспроводного канала
+  
+  // Создание модели потерь с учётом затуханий 
+  Ptr<LogDistancePropagationLossModel> logDistanceLossModel = CreateObject<LogDistancePropagationLossModel>();
+  logDistanceLossModel->SetAttribute("Exponent", DoubleValue(3.0)); // Показатель затухания (3.0 для городской среды)
+  logDistanceLossModel->SetAttribute("ReferenceLoss", DoubleValue(46.6777)); // Потери на 1м для 868MHz
+  
+  // Случайные потери (замирания)
+  Ptr<RandomPropagationLossModel> randomLossModel = CreateObject<RandomPropagationLossModel>();
+  randomLossModel->SetAttribute("Variable", DoubleValue(5.0)); // Случайные колебания ±5dB
+  logDistanceLossModel->SetNext(randomLossModel);
+  
+  // Модель задержки сигнала
+  Ptr<ConstantSpeedPropagationDelayModel> delayModel = CreateObject<ConstantSpeedPropagationDelayModel>();
+
+  // Канал с этими моделями
+  Ptr<WirelessChannel> wirelessChannel = CreateObject<WirelessChannel>();
+  wirelessChannel->SetAttribute("PropagationLossModel", PointerValue(logDistanceLossModel));
+  wirelessChannel->SetAttribute("PropagationDelayModel", PointerValue(delayModel));
+
+  // Настройка мобильности (размещение и передвижение узлов)
   MobilityHelper mobility;
   
   // Шлюз в центре
   Ptr<ListPositionAllocator> positionAllocGateways = CreateObject<ListPositionAllocator> ();
-  positionAllocGateways->Add (Vector (0.0, 0.0, 15.0)); // Высота 15м
+  positionAllocGateways->Add (Vector (0.0, 0.0, 15.0));
   mobility.SetPositionAllocator (positionAllocGateways);
   mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
   mobility.Install (gateways);
@@ -46,25 +69,36 @@ int main (int argc, char *argv[])
   mobilityEd.SetPositionAllocator ("ns3::UniformDiscPositionAllocator",
                                   "X", DoubleValue (0.0),
                                   "Y", DoubleValue (0.0),
-                                  "rho", DoubleValue (2000.0)); // Радиус 2000м
+                                  "rho", DoubleValue (2000.0));
   mobilityEd.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
   mobilityEd.Install (endDevices);
 
-  //Создание LORAWAN стека
-  // LoRa физический уровень
+  
+  // Настройка PHY с учетом шума
+  
+  // Создание помощника для PHY с каналом
   PhyLoraPropModelHelper phyHelper;
-  phyHelper.SetFrequency(868e6); // EU 868 MHz
+  phyHelper.SetChannel(wirelessChannel);
+  phyHelper.SetFrequency(frequency);
+  phyHelper.SetAttribute("NoiseFigure", DoubleValue(noiseFigure)); // Добавляем коэффициент шума
+  phyHelper.SetAttribute("TxPower", DoubleValue(14)); // Мощность передачи по умолчанию
+  
+  // Альтернативный вариант: использование более продвинутой модели канала
+  // Лучше использовать для большего количества устройств
+  // Ptr<ThreeLogDistancePropagationLossModel> lossModel = CreateObject<ThreeLogDistancePropagationLossModel>();
+  // lossModel->SetAttribute("Exponent0", DoubleValue(1.0));  // Ближняя зона
+  // lossModel->SetAttribute("Exponent1", DoubleValue(2.0));  // Средняя зона  
+  // lossModel->SetAttribute("Exponent2", DoubleValue(3.5));  // Дальняя зона
 
   // LoRa MAC уровень
   LorawanMacHelper macHelper;
-  macHelper.SetRegion(LorawanMacHelper::EU); // Европейский регион
+  macHelper.SetRegion(LorawanMacHelper::EU);
 
   // LoRaWAN helper
   LorawanHelper helper;
-  helper.EnablePacketTracking(); // Включаем отслеживание пакетов
+  helper.EnablePacketTracking();
 
   // Установка LoRa на устройства
-  // Настройка для конечных устройств
   macHelper.SetDeviceType(LorawanMacHelper::ED_A);
   helper.Install(phyHelper, macHelper, endDevices);
 
@@ -80,19 +114,19 @@ int main (int argc, char *argv[])
     
     // Устанавливаем разные параметры для каждого устройства
     switch(i) {
-      case 0: // Устройство 1 - близко к шлюзу
+      case 0: // Близко к шлюзу
         edMac->SetDataRate(5);  // SF7
-        edMac->SetTransmissionPower(14); // dBm
+        edMac->SetTransmissionPower(14);
         NS_LOG_INFO("Устройство 0: SF7, мощность 14dBm");
         break;
-      case 1: // Устройство 2 - среднее расстояние
+      case 1: // Среднее расстояние
         edMac->SetDataRate(3);  // SF9
-        edMac->SetTransmissionPower(10); // dBm
+        edMac->SetTransmissionPower(10);
         NS_LOG_INFO("Устройство 1: SF9, мощность 10dBm");
         break;
-      case 2: // Устройство 3 - далеко от шлюза
+      case 2: // Далеко от шлюза
         edMac->SetDataRate(1);  // SF11
-        edMac->SetTransmissionPower(6);  // dBm
+        edMac->SetTransmissionPower(6);
         NS_LOG_INFO("Устройство 2: SF11, мощность 6dBm");
         break;
     }
@@ -103,7 +137,6 @@ int main (int argc, char *argv[])
   PeriodicSenderHelper appHelper = PeriodicSenderHelper ();
   appHelper.SetPeriod (Seconds (appPeriod));
   
-  // Устанавливаем разные размеры пакетов для разных устройств
   Ptr<RandomVariableStream> rv = CreateObjectWithAttributes<UniformRandomVariable> (
     "Min", DoubleValue (10), "Max", DoubleValue (50));
   appHelper.SetPacketSizeRandomVariable (rv);
@@ -118,12 +151,17 @@ int main (int argc, char *argv[])
   networkServerHelper.SetEndDevices (endDevices);
   networkServerHelper.Install (gateways);
 
-  // Установка сетевых приложений
   ApplicationContainer serverContainer;
   ForwarderHelper forwarderHelper;
   forwarderHelper.Install (gateways);
 
-  // Запуск симмуляции
+  // Дополнительная информация о канале
+  NS_LOG_INFO("Настройки беспроводного канала:");
+  NS_LOG_INFO("- Коэффициент шума: " << noiseFigure << " dB");
+  NS_LOG_INFO("- Частота: " << frequency/1e6 << " MHz");
+  NS_LOG_INFO("- Показатель затухания: 3.0 (городская среда)");
+
+  // Запуск симуляции
   NS_LOG_INFO("Запускаем симуляцию на " << simulationTime << " секунд");
   Simulator::Stop (appStopTime + Hours (1));
   Simulator::Run ();
@@ -137,71 +175,7 @@ int main (int argc, char *argv[])
   
   for (int i = 0; i < nDevices; i++) {
     Ptr<Node> node = endDevices.Get(i);
-    Vector pos = node->GetObject<MobilityModel>()->GetPosition();// Простейшая сеть на 2 узла в ns-3
-#include "ns3/core-module.h"
-#include "ns3/network-module.h"
-#include "ns3/internet-module.h"
-#include "ns3/point-to-point-module.h"
-#include "ns3/applications-module.h"
-
-using namespace ns3;
-
-int main(int argc, char *argv[])
-{
-  // Включение логирования
-  Time::SetResolution(Time::NS);
-  LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
-  LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
-
-  std::cout << "Создаем сеть из 2 узлов..." << std::endl;
-
-  // Создание узла 2
-  NodeContainer nodes;
-  nodes.Create(2);
-
-  // Создание каналов связи
-  PointToPointHelper pointToPoint;
-  pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
-  pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
-
-  // Уставновка сестевых интерфейсов
-  NetDeviceContainer devices;
-  devices = pointToPoint.Install(nodes);
-
-  // Установка стека протоколов TCP/IP
-  InternetStackHelper stack;
-  stack.Install(nodes);
-
-  // Назначение IP-адрессов
-  Ipv4AddressHelper address;
-  address.SetBase("10.1.1.0", "255.255.255.0");
-  Ipv4InterfaceContainer interfaces = address.Assign(devices);
-
-  // Создание сервера во втором узле
-  UdpEchoServerHelper echoServer(9); // Порт 9
-  ApplicationContainer serverApps = echoServer.Install(nodes.Get(1));
-  serverApps.Start(Seconds(1.0));
-  serverApps.Stop(Seconds(10.0));
-
-  // Создание клиента в первом узле
-  UdpEchoClientHelper echoClient(interfaces.GetAddress(1), 9);
-  echoClient.SetAttribute("MaxPackets", UintegerValue(3));
-  echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
-  echoClient.SetAttribute("PacketSize", UintegerValue(512));
-
-  ApplicationContainer clientApps = echoClient.Install(nodes.Get(0));
-  clientApps.Start(Seconds(2.0));
-  clientApps.Stop(Seconds(6.0));
-
-  std::cout << "Дительность симмуляции 10 секунд" << std::endl;
-
-  // Запуск симмуляции
-  Simulator::Run();
-  Simulator::Destroy();
-
-  std::cout << "Симуляция завершена!" << std::endl;
-  return 0;
-}
+    Vector pos = node->GetObject<MobilityModel>()->GetPosition();
     NS_LOG_INFO("Устройство " << i << " позиция: (" << pos.x << ", " << pos.y << ")");
   }
 
